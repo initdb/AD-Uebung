11b446e35421e38cc7c49ee40f131ca204fdb9a8
sEITbtschw <benedict.schwind@stud.fh-rosenheim.de>
2018-10-07 19:09:19 +0200

    initial commit of sources

---
+++ uebung1.project/src/main/java/BubbleSort.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] a = {35, 22, 10, 51, 48};
+        System.out.println("Eingabe: " + Arrays.toString(a));
+        bubbleSort(a);
+        System.out.println("Ausgabe: " + Arrays.toString(a));
+    }
+
+    public static void bubbleSort (int[] a) {
+        // TODO
+    }
+}
