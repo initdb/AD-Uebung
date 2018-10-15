c4f9fb0099f2394ba850fe80fecb6c54b1c000a7
initdb <benedict.schwind@live.de>
2018-10-15 20:50:09 +0200

    fix Aktienkurs ?

--- uebung2.project/src/main/java/Aktienkurse.java
+++ uebung2.project/src/main/java/Aktienkurse.java
@@ -4,44 +4,41 @@
 public class Aktienkurse {
 
     // absolute Kurs
-    public static final int[] changes = {1, -5, 4, 2, -7, 3, 6, -1, 2, -4, 7, -10, 2, 6, 1, -3};
+    public static final int[] changes = /*{1, -5, 4, 2, -7, 3, 6, -1, 2, -4, 7, -10, 2, 6, 1, -3}*/{5,-6,3};
 
 
     public static void main(String[] args) {
 
-        Integer maxSum = 0/*Integer.MIN_VALUE*/; // keeps sum of maximum subarray (that is already known at current position)
+        Integer maxSum = Integer.MIN_VALUE; // keeps sum of maximum subarray (that is already known at current position)
         int low = 0;                        // - left index
         int high = 0;                       // - right index
 
 
-        Integer curSum = 0/*Integer.MIN_VALUE*/; // keeps maximum sum of all subarrays that end at current position
+        Integer curSum = Integer.MIN_VALUE; // keeps maximum sum of all subarrays that end at current position
         int curLow = 0;                     // - left index
-        int curHigh = 0;
+        // braucht man gar nicht. int curHigh = 0;
 
         for (int pos = 0; pos < changes.length; pos++)
         {   // scanline: read from left to right
-            curSum += changes[pos];
             if(curSum > 0)
             {
                 // curSum aktualisieren
-                //curSum += changes[pos];
-                curHigh = pos;
+                curSum += changes[pos];
             }
 		    else
             {
                 // EdgeMaxSubarray neu beginnnen
                 // nur aktuelles Element bildet EdgeMaxSubArray
-                curSum = 0;
-                curLow = pos + 1;
-                curHigh = pos;
+                curSum = changes[pos];
+                curLow = pos;
             }
 
-            if(curSum > maxSum)
+            if(curSum > maxSum && pos > high)
             {
                 // EdgeMaxSubarray ist besser als bisher
                 // gefundenes MaximumSubArray
                 maxSum = curSum;
-                high = curHigh;
+                high = pos;
                 low = curLow;
             }
         }
